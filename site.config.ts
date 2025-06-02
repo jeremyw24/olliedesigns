8c92ce5a994ef20dcfa06aa74d3aff12d9ca72cd
jeremyw24 <jeremy@visionswest.com.au>
2025-06-02 16:12:41 +1000

    various updates

--- site.config.ts
+++ site.config.ts
@@ -1,5 +1,5 @@
-export const siteUrl = "https://mizar.majestico.co";
-export const siteTitle = "Mizar";
+export const siteUrl = "https://olliedesigns.com.au";
+export const siteTitle = "Ollie Designs";
 export const defaultLocale = "en";
-export const locales = ["en", "it"];
-export type Locale = "en" | "it";
+export const locales = ["en"];
+export type Locale = "en";
