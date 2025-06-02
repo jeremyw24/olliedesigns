b82fd1c9d1edacd12e81ec25a9bd378ffee20bd0
jeremyw24 <jeremy@visionswest.com.au>
2025-06-02 16:16:16 +1000

    Remove Italian localization files and update localization settings to support only English.

--- src/lib/localization-helpers.ts
+++ src/lib/localization-helpers.ts
@@ -2,30 +2,19 @@ import contacts from "@/content/global/contacts.json";
 import footerEn from "@/content/global/en/footer.json";
 import headerEn from "@/content/global/en/header.json";
 import seoEn from "@/content/global/en/seo.json";
-import footerIt from "@/content/global/it/footer.json";
-import headerIt from "@/content/global/it/header.json";
-import seoIt from "@/content/global/it/seo.json";
 import style from "@/content/global/style.json";
 import widget from "@/content/global/widget.json";
 import { defaultLocale, locales } from "site.config";
 
 const settings: Record<string, LocalizedSettings> = {
-	it: {
-		header: headerIt,
-		footer: footerIt,
-		contacts: contacts,
-		seo: seoIt,
-		style: style,
-		widget: widget,
-	},
-	en: {
-		header: headerEn,
-		footer: footerEn,
-		contacts: contacts,
-		seo: seoEn,
-		style: style,
-		widget: widget,
-	},
+en: {
+header: headerEn,
+footer: footerEn,
+contacts: contacts,
+seo: seoEn,
+style: style,
+widget: widget,
+},
 };
 
 export function getLocalizedSettings(locale?: string): LocalizedSettings {
